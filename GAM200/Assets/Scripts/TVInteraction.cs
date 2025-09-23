345d427f8c6b8e12c99513188da89a696ded3335
rsnhma <rsnhma2002@gmail.com>
2025-09-24 02:34:13 +0800

    added vhs and tv interaction

---
+++ GAM200/Assets/Scripts/TVInteraction.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class TVInteraction : MonoBehaviour
+{
+    private bool playerNearby = false;
+    [SerializeField] private TextMeshProUGUI interactText;
+
+    [Header("Enemy Spawn")]
+    public MainEnemy enemyPrefab;   // Assign your enemy prefab here
+    public Transform spawnPoint;    // Position of TV (enemy crawls out here)
+
+    private void Update()
+    {
+        if (playerNearby && VHSItem.hasVHS)
+        {
+            interactText.text = "Left Click to put tape in";
+            interactText.gameObject.SetActive(true);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(HandleCutscene());
+            }
+        }
+        else
+        {
+            interactText.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator HandleCutscene()
+    {
+        Debug.Log("Tape inserted. Cutscene start (skipped for now)");
+
+        interactText.gameObject.SetActive(false);
+
+        // TODO: Play actual cutscene here
+        yield return new WaitForSeconds(2f); // buffer time before chase
+
+        SpawnEnemy();
+    }
+
+    private void SpawnEnemy()
+    {
+        if (enemyPrefab != null && spawnPoint != null)
+        {
+            MainEnemy enemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+            enemy.BeginChase();
+            Debug.Log("Enemy spawned and begins chase!");
+        }
+        else
+        {
+            Debug.LogError("EnemyPrefab or SpawnPoint not set on TVInteraction");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            playerNearby = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+            interactText.gameObject.SetActive(false);
+        }
+    }
+}
